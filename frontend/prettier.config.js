baed24b6211377bfc65edfffd5eb9e3c5a4df226
Syknapse <29199184+Syknapse@users.noreply.github.com>
2022-12-02 13:49:05 +0100

    Basic setup
    Add basic outline components.
    Add Prettier and config.
    Add clsx, prep for CSS module use.

---
+++ frontend/prettier.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  htmlWhitespaceSensitivity: 'css',
+  insertPragma: false,
+  jsxSingleQuote: false,
+  printWidth: 120,
+  proseWrap: 'preserve',
+  quoteProps: 'as-needed',
+  requirePragma: false,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  useTabs: false,
+}
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,41 @@
-import React from "react";
-import "./App.css";
+import React from 'react'
+import styles from './App.module.css'
+
+const entries = [
+  {
+    year: 1991,
+    type: 'life event',
+    title: 'a chicken was born',
+  },
+  {
+    year: 2000,
+    type: 'Home',
+    title: 'MOved to Grimsby',
+  },
+]
 
 function App() {
-  return <div className="App">TIMELINE App</div>;
+  return (
+    <div className={styles.App}>
+      <header>
+        <h1>Timeline</h1>
+        <div className={styles.filters}>
+          <button>Sort</button>
+          <button>View</button>
+          <button>Time Since</button>
+        </div>
+      </header>
+      <main>
+        {entries.map(entry => (
+          <section>
+            <div>{entry.year}</div>
+            <div>{entry.type}</div>
+            <div>{entry.title}</div>
+          </section>
+        ))}
+      </main>
+    </div>
+  )
 }
 
-export default App;
+export default App
