a8dbaa4fb7d85720912dece9d3634b219133795e
Syknapse <29199184+Syknapse@users.noreply.github.com>
2023-01-03 11:34:31 +0100

    Make modals more keyboard accessible

--- frontend/src/components/Modal/modal.tsx
+++ frontend/src/components/Modal/modal.tsx
@@ -1,5 +1,8 @@
+import { useRef, useEffect } from 'react'
 import clsx from 'clsx'
 import styles from './modal.module.css'
+import { Button } from '../../components'
+import { Cross } from '../../icons'
 
 interface IModalProps {
   className?: string
@@ -9,11 +12,29 @@ interface IModalProps {
 }
 
 const Modal: React.FC<IModalProps> = ({ className, isOpen, close, children }) => {
+  const containerRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (containerRef.current) containerRef.current.focus()
+  }, [])
+
   if (!isOpen) return null
   return (
     <div className={clsx(className, styles.root, isOpen && styles.open)}>
-      <div className={styles.backdrop} onClick={close}></div>
-      <div className={styles.container}>{children}</div>
+      <div className={styles.backdrop} onClick={close}>
+        <Button
+          className={styles.button}
+          onClick={e => {
+            e?.stopPropagation()
+            close()
+          }}
+        >
+          <Cross />
+        </Button>
+      </div>
+      <div className={styles.container} ref={containerRef} tabIndex={-1}>
+        {children}
+      </div>
     </div>
   )
 }
